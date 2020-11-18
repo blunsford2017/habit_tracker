9bdf8bd43013dde267f0a35bdae7a9765227c2be
B.Lunsford <breanna.sirbin@gmail.com>
2020-11-17 22:28:25 -0700

    adds authorization and some server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+// Require modules
+const express = require('express');
+const morgan = require('morgan');
+const port = 3000;
+const indexRouter = require('./routes/index');
+const session = require('express-session');
+const userRouter = require('./routes/users');
+const authoriztion = require('./utils/authorization');
+
+// Set up express app
+const app = express();
+
+// connect to DB
+require('./config/database');
+
+// configurethe app with app.set()
+app.set('view engine', 'ejs');
+
+// Mount middleware with app.use()
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// mounting authorization
+app.use(authoriztion.addUserToRequest);
+
+// Mount routes with app.use()
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+
+// Tell App to listen
+app.listen(port, function() {
+    console.log (`Express is listening on port: ${port}`);
+});
---
+++ utils/authorization.js
@@ -0,0 +1,31 @@
+// require the user model
+const User = require('../models/user');
+
+// set up modle.exports
+module.exports = {
+    addUserToRequest,
+    isAuthenticated,
+}
+
+// a function to add the user to a property called user on the requests object
+// aka req.user
+function addUserToRequest(req, res, next) {
+    // check if user is added to request
+if (req.user) return next(); 
+    // check to see if there is a session created
+    if(req.session && req.session.userId){
+        // find the user based on there id and then add them to the request object
+        User.findById(req.session.userId, function(err, foundUser) {
+            req.user = foundUser;
+            next();
+        });
+    } else {
+        next();
+    };
+};
+
+// a function to check if a user is authenticated
+function isAuthenticated(req, res, next) {
+    if (req.user) return next(); //there is an authenticated user
+    res.redirect('/users/signin'); //send them to the login page
+}
